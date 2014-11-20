44fc875594074400e6f8a37beb4242331bcb0af5
Nam <nam.profittask@gmail.com>
2014-11-20 15:55:32 +0700

    Begin_SMTeam Project

---
+++ bookshop_V1.0/src/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author K55VM
+ */
+import controller.Login_ctr;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        new Login_ctr();
+    }
+}
