ffbf8b89b198ab71512076e47d928285c7afcc90
Nguyen Trong Nghia <nguyentrongnghia.spa@gmail.com>
2014-11-21 08:19:03 +0700

    Database v2

--- bookshop.sql
+++ bookshop.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 20, 2014 at 10:21 AM
--- Server version: 5.5.39
--- PHP Version: 5.4.31
+-- Generation Time: Nov 21, 2014 at 02:16 AM
+-- Server version: 5.6.20
+-- PHP Version: 5.5.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -69,7 +69,11 @@ INSERT INTO `bill` (`billid`, `id`, `date`) VALUES
 ('BID04', 'tuan', '2014-11-20 03:09:21'),
 ('BID05', 'hien', '2014-05-19 04:33:47'),
 ('BID06', 'quy', '2013-12-11 08:36:03'),
-('BID07', 'nghia.nt', '2012-10-09 00:37:27');
+('BID07', 'nghia.nt', '2012-10-09 00:37:27'),
+('BID08', 'tuan', '2013-11-13 03:40:55'),
+('BID09', 'tuan', '2014-10-15 01:14:45'),
+('BID10', 'hien', '2014-10-23 03:14:06'),
+('BID11', 'quy', '2013-12-18 08:21:24');
 
 -- --------------------------------------------------------
 
@@ -90,13 +94,52 @@ CREATE TABLE IF NOT EXISTS `billdetail` (
 INSERT INTO `billdetail` (`billid`, `bookid`, `amountsell`) VALUES
 ('BID01', 'B01', 2),
 ('BID01', 'B04', 1),
-('BID02', 'B02', 1),
+('BID01', 'B07', 1),
+('BID02', 'B02', 3),
 ('BID03', 'B03', 2),
+('BID03', 'B05', 2),
 ('BID04', 'B01', 1),
 ('BID04', 'B02', 1),
+('BID04', 'B03', 2),
+('BID04', 'B06', 5),
 ('BID05', 'B06', 5),
+('BID06', 'B02', 2),
 ('BID06', 'B05', 3),
-('BID07', 'B07', 32);
+('BID07', 'B04', 1),
+('BID07', 'B07', 10),
+('BID08', 'B05', 2),
+('BID09', 'B06', 7),
+('BID10', 'B04', 1),
+('BID10', 'B06', 4),
+('BID10', 'B07', 3),
+('BID11', 'B01', 3),
+('BID11', 'B02', 2),
+('BID11', 'B03', 4);
+
+--
+-- Triggers `billdetail`
+--
+DELIMITER //
+CREATE TRIGGER `amountsell_delete` BEFORE DELETE ON `billdetail`
+ FOR EACH ROW UPDATE book as b
+SET b.amount = b.amount + OLD.amountsell
+WHERE b.bookid = OLD.bookid
+//
+DELIMITER ;
+DELIMITER //
+CREATE TRIGGER `amountsell_insert` BEFORE INSERT ON `billdetail`
+ FOR EACH ROW UPDATE book as b
+SET b.amount = b.amount - NEW.amountsell
+WHERE b.bookid = NEW.bookid
+//
+DELIMITER ;
+DELIMITER //
+CREATE TRIGGER `amountsell_update` BEFORE UPDATE ON `billdetail`
+ FOR EACH ROW UPDATE book as b
+SET b.amount = b.amount + (OLD.amountsell - NEW.amountsell)
+WHERE b.bookid = NEW.bookid AND NEW.bookid = OLD.bookid
+//
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -117,13 +160,13 @@ CREATE TABLE IF NOT EXISTS `book` (
 --
 
 INSERT INTO `book` (`bookid`, `bookname`, `categoryid`, `amount`, `unitprice`) VALUES
-('B01', 'Math 12', 'C02', 50, 30000),
+('B01', 'Math 12', 'C02', 47, 30000),
 ('B02', 'Gone With The Wind', 'C01', 20, 150000),
-('B03', 'A Little Princess', 'C03', 10, 55000),
-('B04', 'History 12', 'C02', 20, 20000),
-('B05', 'One Thousand and One Nights', 'C01', 17, 1000000),
-('B06', 'The Princess and the Pea', 'C03', 67, 21700),
-('B07', 'Book of Science', 'C02', 60, 320000);
+('B03', 'A Little Princess', 'C03', 24, 55000),
+('B04', 'History 12', 'C02', 24, 20000),
+('B05', 'One Thousand and One Nights', 'C01', 38, 1000000),
+('B06', 'The Princess and the Pea', 'C03', 56, 21700),
+('B07', 'Book of Science', 'C02', 56, 320000);
 
 -- --------------------------------------------------------
 
